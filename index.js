091af273649c807e89e6466a1023a11982daf606
ss0311 <soichiroseike@gmail.com>
2021-09-12 23:01:08 +0900

    Enable to post Photos.

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 const { ApolloServer } = require(`apollo-server`)
 
 const typeDefs = `
+  type Photo {
+      id: ID!
+      url: String!
+      name: String!
+      description: String
+      category: PhotoCategory!
+  }
+
+  enum PhotoCategory {
+      SERFIE
+      PORTRAIT
+      ACTION
+      LANDSCAPE
+      GRAPHIC
+  }
+
+  input PostPhotoInput {
+      name: String!
+      category: PhotoCategory=PORTRAIT
+      description: String
+  }
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
+  }
+
+  type Mutation {
+      postPhoto(input: PostPhotoInput!): Photo!
   }
 `
+
+var photos = []
+
 const resolvers = {
     Query: {
-        totalPhotos: () => 42
+        totalPhotos: () => photos.length,
+        allPhotos: () => photos
+    },
+
+    Mutation: {
+        postPhoto(parent, args) {
+            var input = args.input
+            var photo = {
+                id: photos.length,
+                ...args.input
+            }
+            photos.push(photo)
+            return photo
+        }
+    },
+
+    Photo: {
+        url: parent => `http://yoursite.com/img/${parent.id}.jpg`
     }
 }
 
